03091f0119eb5d999e8cc63b6df37e60dd232aeb
ssatocc <ssatocc@gmail.com>
2024-02-04 22:43:17 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+import os
+import random
+
+from redis import Redis
+
+
+def main():
+    r = Redis(
+        host=os.getenv("REDIS_HOST", "localhost"),
+        port=os.getenv("REDIS_PORT", 6379),
+        decode_responses=True
+    )
+    for _ in range(random.randint(1, 10)):
+        count = r.incr("count")
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
